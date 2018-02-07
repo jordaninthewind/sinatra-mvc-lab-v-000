5a69e4a464e901b6a981d3e209134f8d4bdc775e
Jordan Kline <jordan.kline@gmail.com>
2018-02-07 19:05:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    # @word = params[:user_phrase]
+
+    @piggy_word = PigLatinizer.new(params[:user_phrase])#.to_pig_latin
+    binding.pry
+    erb :piglatinize
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,30 @@
+class PigLatinizer
+  attr_accessor :word
+
+  @@vowels = 'aeiouAEIOU'
+
+  def initialize(word = nil)
+    @word = word
+  end
+
+  def piglatinize(word)
+    if @@vowels.include?(word.chars.first)
+      return word + "way"
+    else
+      vowel = self.first_vowel(word)
+      first_syl = word.split(vowel[0])[0]
+      base = word.chars[first_syl.length..-1]
+      base << first_syl << "ay"
+      return base.join("")
+    end
+  end
+
+  def first_vowel(word)
+    word.chars.select{|el| @@vowels.include?(el)}
+  end
+
+  def to_pig_latin(phrase)
+    phrase.split.collect {|el| self.piglatinize(el)}.join(" ")
+  end
+
+end
